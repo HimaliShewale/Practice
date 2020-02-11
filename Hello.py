d93d182c418591afa47d8b6949fde3f8d79156e4
HimaliShewale <35599372+HimaliShewale@users.noreply.github.com>
2020-02-11 23:02:44 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,10 @@
+#Hello World
+
+print('Hello World')
+print('What is your name?')
+myName=input()
+print('Hey Nice to Meet you,' +myName)
+print(len(myName))
+print('What is your age?')
+myAge=input()
+print('Wow your age is,' +myAge)
